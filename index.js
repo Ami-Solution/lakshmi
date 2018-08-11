d35dc14c0c89f1b50390adaf9aac5dcde4012255
nadir <nadirlaskar@gmail.com>
2018-08-12 01:59:10 +0530

    front-end all screens added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var models = [];
+var obj = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+fs.writeFileSync('./data.json', models.join(',') , 'utf-8'); 
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -8,5 +12,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 
 app.use(express.static('public'));
+app.get("/:data",(req,res)=>{
+   res.sendfile(__dirname+"/public/index.html");
+})
 PORT = 3000;
 app.listen(process.env.PORT || PORT, () => console.log(`Durga app listening on port ${PORT}!`));
--- public/script/main.js
+++ public/script/main.js
@@ -1,11 +1,38 @@
 M.AutoInit();
 
-const OTPModel = {
-    phone: ""
+$('.chips-placeholder').chips({
+    placeholder: 'Type here',
+    secondaryPlaceholder: '+ Add More',
+});
+
+var data = window.location.pathname.substr(1);
+
+if(data!=""){
+    //Verify data and get loan info
+    $($(".breadcrumb")[0]).remove();
+    $($(".breadcrumb")[0]).remove();
+    $($(".breadcrumb")[0]).addClass("active");
+    $(".onboard-step-1").addClass("hide");
+    $(".onboard-step-5").removeClass("hide");
+    $($(".breadcrumb")[1]).removeClass("hide");
+
+}else{
+    $($(".breadcrumb")[0]).addClass("active");
 }
-$("#phone").on('keypress',(e)=>{
-    OTPModel.phone+=e.key;
-    if(OTPModel.phone.length>9){
+
+$('input#idea-details, textarea#idea-details').characterCounter();
+
+const OTPModel = {
+    phone: "",
+    OTP: ""
+}
+$("#phone").on('focus',()=>{
+    $("#phone").val('+91');
+});
+
+$("#phone").on('keyup',(e)=>{
+    OTPModel.phone = $("#phone").val();
+    if(OTPModel.phone.length==13){
         $("#sendOTP").removeClass("disabled");
     }else{
         $("#sendOTP").addClass("disabled")
@@ -17,4 +44,64 @@ $("#sendOTP").on("click",()=>{
     $("#number").text(OTPModel.phone);
     $("#otp-card").addClass("hide");
     $("#otp-verify-card").removeClass("hide");
+});
+
+$("#otp").on('keypress',(e)=>{
+    OTPModel.OTP+=e.key;
+    if(OTPModel.OTP.length>4){
+        $("#verifyOTP").removeClass("disabled");
+    }else{
+        $("#verifyOTP").addClass("disabled")
+    }
+});
+
+$("#verifyOTP").on("click",()=>{
+    M.toast({html:`Verifying OTP Sent to ${OTPModel.phone}`});
+    setTimeout(()=>{   
+        $("#otp-verify-card").addClass("hide");
+        $($(".step .breadcrumb")[0]).remove();
+        $($(".step .breadcrumb")[0]).addClass("active");
+        $(".onboard-step-2").removeClass("hide");
+        $(".onboard-step-1").addClass("hide");
+        $(".step .breadcrumb").removeClass("hide");
+        M.toast({html:`OTP Verified.`});
+    },5000)
+});
+
+$("#next").on("click",()=>{
+    $(".onboard-step-2").addClass("hide");
+    $(".onboard-step-3").removeClass("hide");
+})
+
+$("#edit-story").on("click",()=>{
+    $(".onboard-step-3").addClass("hide");
+    $(".onboard-step-2").removeClass("hide");
+})
+
+$("#submit-review").on("click",()=>{
+    M.toast({html:"Loan application submited for review, hang tight !"})
+    $(".onboard-step-3").addClass("hide");
+    $(".onboard-step-4").removeClass("hide");
+})
+
+$("#edit-info").on("click",()=>{
+    $(".onboard-step-4").addClass("hide");
+    $(".onboard-step-3").removeClass("hide");
+})
+
+$("#accept-offer-term").on("click",()=>{
+    $(".onboard-step-5").addClass("hide");
+    $(".onboard-step-6").removeClass("hide");
+})
+
+$("#back-to-step-5").on("click",()=>{
+    $(".onboard-step-6").addClass("hide");
+    $(".onboard-step-5").removeClass("hide");
+})
+
+$("#confirm-offer-term").on("click",()=>{
+    $($(".step .breadcrumb")[0]).removeClass("active");
+    $($(".step .breadcrumb")[1]).addClass("active");
+    $(".onboard-step-6").addClass("hide");
+    $(".onboard-step-7").removeClass("hide");
 })
