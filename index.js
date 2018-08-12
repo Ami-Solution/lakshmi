f99713a97e756e176a0c249900757c5eb9d516a5
nadir <nadirlaskar@gmail.com>
2018-08-12 14:59:39 +0530

    integrated model to frontend

--- index.js
+++ index.js
@@ -2,18 +2,184 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var models = [];
-var obj = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
-fs.writeFileSync('./data.json', models.join(',') , 'utf-8'); 
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+const SendOtp = require('sendotp');
+const sendOtp = new SendOtp('231274A7Y7SKOlQx5b6f45b4');
+sendOtp.setOtpExpiry('5');
+
+
+var models = {};
+loadModels =  function(){
+  models = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+}
+loadModels();
+
+saveModels = function(){
+  setTimeout(()=>{
+    fs.writeFileSync('./data.json', JSON.stringify(models) , 'utf-8'); 
+  },0);
+}
+
+var pendingReviews = {};
+loadPendingReviews =  function(){
+  pendingReviews = JSON.parse(fs.readFileSync('./pendingReviews.json', 'utf8'));
+}
+loadPendingReviews();
+
+savePendingReviews = function(){
+  setTimeout(()=>{
+    fs.writeFileSync('./pendingReviews.json', JSON.stringify(models) , 'utf-8'); 
+  },0);
+}
+
+var modelNode = {
+  loan:{
+     approved: false,
+     confirmed: false,
+     amount: 0,
+     emi: 0,
+     term: "",
+     nextPaymenDate: "",
+     paymentLink: ""
+  },
+  details:{
+     phone: "",
+     story:"",
+     amount:"",
+     images: []
+  },
+  otherInfo:{
+     name:"",
+     address:"",
+     angels:[],
+     documents: []
+  }
+};
+
+modelNode.new = ()=>{
+  return JSON.parse(JSON.stringify(modelNode));
+}
+
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(express.static('public'));
 app.get("/:data",(req,res)=>{
-   res.sendfile(__dirname+"/public/index.html");
-})
+   res.sendFile(__dirname+"/public/index.html");
+});
+
+app.get("/api/getOTP/:number",(req,res)=>{
+  var number = req.params.number;
+  sendOtp.send(number, "DUROTP", function (error, status, otp_res) {
+    var response = {
+      otp_status: (status&&status.type == "success"),
+      phone_number: number,
+      error: error
+    };
+    res.send(response);
+  });
+});
+
+app.get("/getModel/:number",(req,res)=>{
+    loadModels();
+    loadPendingReviews();
+    if(pendingReviews[req.params.number])
+      res.send(pendingReviews[req.params.number]);
+    else res.send(models[req.params.number]);
+});
+
+app.post("/saveModel",(req,res)=>{
+  var model = req.body;
+  models[model.details.phone] = model;
+  if(pendingReviews[model.details.phone]&&pendingReviews[model.details.phone].loan.approved){
+    pendingReviews[model.details.phone] = model;
+    savePendingReviews();
+  }
+  saveModels();
+  res.send(models[model.details.phone]);
+});
+
+app.post("/submitReview",(req,res)=>{
+  var model = req.body;
+  models[model.details.phone] = model;
+  saveModels();
+  pendingReviews[model.details.phone] = models[model.details.phone];
+  savePendingReviews();
+  res.send(pendingReviews[model.details.phone]);
+});
+
+app.get("/loan/:id",(req,res)=>{
+    var id = base64toString(req.params.id);
+    res.send(pendingReviews[id]);
+});
+
+app.get("/confirm/loan/:id",(req,res)=>{
+  var id = base64toString(req.params.id);
+  pendingReviews[id].loan.confirmed = true;
+  res.send(pendingReviews[id]);
+});
+
+app.get("/api/verifyOtp/:phone_number/:otp", function (req, res) {
+  var otp = req.params.otp;
+  var number =  req.params.phone_number;
+
+  sendOtp.verify(number, otp, function (err, data, response) {
+    var response = {
+      status: data&&data.type == 'success'
+    }
+
+    if(response.status){
+      models[number] = modelNode.new();
+      models[number].details.phone = number;
+      saveModels();
+    }
+    res.send(response);
+  });
+});
+
 PORT = 3000;
 app.listen(process.env.PORT || PORT, () => console.log(`Durga app listening on port ${PORT}!`));
+
+
+function base64toString(s) {
+  console.log(s);
+  if (!s) return s;
+  else return Buffer.from(s, 'base64').toString();
+}
+
+function getObjectFromBase64(data) {
+  try {
+    console.log(base64toString(data));
+    return JSON.parse(base64toString(data));
+  } catch (ex) {
+    return {}
+  }
+}
+
+/**
+ * {
+ *   "ph":{
+ *       loan:{
+ *          approved: false,
+ *          amount: 0,
+ *          emi: 0,
+ *          term: "",
+ *          nextPaymenDate: "",
+ *          paymentLink: ""
+ *       },
+ *       details:{
+ *          phone: "",
+ *          story:"",
+ *          amount:"",
+ *          images: []
+ *       },
+ *       otherInfo:{
+ *          name:"",
+ *          address:"",
+ *          angels:[],
+ *          documents: []
+ *       }
+ *    }
+ * }
+ */
--- public/script/main.js
+++ public/script/main.js
@@ -1,31 +1,224 @@
-M.AutoInit();
-
-$('.chips-placeholder').chips({
-    placeholder: 'Type here',
-    secondaryPlaceholder: '+ Add More',
+$.extend({
+    jpost: function(url, body) {
+        return $.ajax({
+            type: 'POST',
+            url: url,
+            data: JSON.stringify(body),
+            contentType: "application/json",
+            dataType: 'json'
+        });
+    }
 });
 
-var data = window.location.pathname.substr(1);
+M.AutoInit();
 
-if(data!=""){
-    //Verify data and get loan info
-    $($(".breadcrumb")[0]).remove();
-    $($(".breadcrumb")[0]).remove();
-    $($(".breadcrumb")[0]).addClass("active");
-    $(".onboard-step-1").addClass("hide");
-    $(".onboard-step-5").removeClass("hide");
-    $($(".breadcrumb")[1]).removeClass("hide");
+var chips = $('.chips-placeholder').chips({
+    placeholder: 'Type here',
+    secondaryPlaceholder: '+ Add More',
+    onChipAdd: (value)=>{
+        value.find("input").data("key",value.data("key"))
+        updateModel.call(value.find("input"));
+    }
+});
 
-}else{
-    $($(".breadcrumb")[0]).addClass("active");
-}
-
-$('input#idea-details, textarea#idea-details').characterCounter();
 
 const OTPModel = {
     phone: "",
     OTP: ""
 }
+
+var Info = {
+    loan:{
+       approved: false,
+       amount: 0,
+       emi: 0,
+       term: "",
+       nextPaymenDate: "",
+       paymentLink: ""
+    },
+    details:{
+       phone: "",
+       story:"",
+       amount:"",
+       images: []
+    },
+    otherInfo:{
+       name:"",
+       address:"",
+       angels:[],
+       documents: []
+    }
+ }
+
+function getModel(){
+   var number = localStorage.getItem("userId");
+   if(number){
+     $.get("/getModel/"+number).then((res)=>{
+        Info = res;
+        processState();
+        prefillFields();
+     }).catch(()=>{
+        console.error("Error loading model");
+     })
+   }else{
+     $(".onboard-step-1").removeClass("hide");
+   }
+}
+
+function saveModel(reload = false){
+    $.jpost("/saveModel",Info,"application/json").then((res)=>{
+        Info = res;
+        if(reload) 
+            processState();
+    }).catch(()=>{
+        M.toast({html:"Error Saving Information"});
+    })
+}
+function prefillFields(){
+    $("*[data-key]").each((id,el)=>{
+        var jelm = $(el);
+        var isFile = false;
+    
+        if($(this).prop("files")){
+            isFile = true;
+        }
+
+    var keys = jelm.data("key");
+
+    if(keys)
+        keys.split(".").reduce((obj,key) => {
+            if(typeof obj[key] == "string"){
+                if(!isFile){
+                    if(jelm.prop("tagName")=="TEXTAREA")
+                        jelm.text(obj[key]);
+                    else jelm.val(obj[key]);
+                }
+                //else saveFile(data,obj,key);
+            }else if(obj[key] instanceof Array){
+                if(!isFile){
+                   if(jelm.hasClass("chips")){
+                    var ins = M.Chips.getInstance(chips);
+                    obj[key].forEach(elm => {
+                        ins.addChip({
+                            tag: elm
+                        });
+                    });
+                   }   
+                }
+                else{
+                    //saveFile(data,obj,key);
+                }
+            }
+            return obj[key];
+        },Info);
+
+    })
+}
+
+function processState(){
+    if(Info.loan.confirmed){
+        $("#offer-amount-1").text(Info.loan.amount);
+        $("#offer-amount-2").text(Info.loan.amount);
+        $("#received-amount").text(Info.loan.amount);
+        $("#offer-emi-1").text(Info.loan.emi);
+        $("#offer-emi-2").text(Info.loan.emi);
+        $("#pay-amount-installment").text(Info.loan.emi);
+        $("#offer-time-1").text(Info.loan.term);
+        $("#offer-time-2").text(Info.loan.term);
+        $("#next-payment-date").text(Info.loan.nextPaymenDate);
+        $(".onboard-step-6").addClass("hide");
+        $(".onboard-step-7").removeClass("hide");
+        $($(".breadcrumb")[0]).remove();
+        if($(".breadcrumb").length==3)
+            $($(".breadcrumb")[0]).remove();
+        $($(".breadcrumb")[1]).removeClass("hide");
+        $($(".breadcrumb")[1]).addClass("active");
+    }
+    else if(Info.loan.approved){
+        $("#offer-amount-1").text(Info.loan.amount);
+        $("#offer-amount-2").text(Info.loan.amount);
+        $("#received-amount").text(Info.loan.amount);
+        $("#offer-emi-1").text(Info.loan.emi);
+        $("#offer-emi-2").text(Info.loan.emi);
+        $("#pay-amount-installment").text(Info.loan.emi);
+        $("#offer-time-1").text(Info.loan.term);
+        $("#offer-time-2").text(Info.loan.term);
+        $("#next-payment-date").text(Info.loan.nextPaymenDate);
+         $($(".breadcrumb")[0]).remove();
+         $($(".breadcrumb")[0]).remove();
+         $($(".breadcrumb")[0]).addClass("active");
+         $(".onboard-step-1").addClass("hide");
+         $(".onboard-step-5").removeClass("hide");
+         $($(".breadcrumb")[1]).removeClass("hide");
+    }
+    else{
+        $("#otp-verify-card").addClass("hide");
+        $($(".step .breadcrumb")[0]).remove();
+        $($(".step .breadcrumb")[0]).addClass("active");
+        $(".onboard-step-2").removeClass("hide");
+        $(".onboard-step-1").addClass("hide");
+        $(".step .breadcrumb").removeClass("hide");
+    }
+}
+
+function updateModel(){
+    var jelm = this.find?this:$(this);
+    var data = $(this).val();
+    var isFile = false;
+    if(!data)
+      data = $(this).text();
+    
+    if($(this).prop("files")){
+        isFile = true;
+        data = $(this).prop("files")[0];
+    }
+
+    var keys = jelm.data("key");
+
+    if(keys)
+        keys.split(".").reduce((obj,key) => {
+            if(typeof obj[key] == "string"){
+                if(!isFile){
+                    obj[key] = data;
+                }
+                else saveFile(data,obj,key);
+            }else if(obj[key] instanceof Array){
+                if(!isFile){
+                    obj[key].push(data);
+                }
+                else{
+                    saveFile(data,obj,key);
+                }
+            }
+            return obj[key];
+        },Info);
+}
+
+function saveFile(file,obj,key){
+    getBase64(file).then((data) => {
+        if(obj[key] instanceof Array)
+            obj[key].push(data);
+        else obj[key] = data;
+        $(".thumbs-wrapper").append(`<img class="circle" src="${data}">`);
+    });
+}
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+$("input,textarea").on("change",(e)=>{
+    updateModel.call(e.currentTarget);
+})
+
+
+$('input#idea-details, textarea#idea-details').characterCounter();
+
 $("#phone").on('focus',()=>{
     $("#phone").val('+91');
 });
@@ -41,14 +234,20 @@ $("#phone").on('keyup',(e)=>{
 
 $("#sendOTP").on("click",()=>{
     M.toast({html:`Sending OTP to ${OTPModel.phone}`});
-    $("#number").text(OTPModel.phone);
-    $("#otp-card").addClass("hide");
-    $("#otp-verify-card").removeClass("hide");
+    $.get("/api/getOTP/"+OTPModel.phone).then((res)=>{
+        if(res.otp_status){        
+            $("#number").text(res.phone_number);
+            $("#otp-card").addClass("hide");
+            $("#otp-verify-card").removeClass("hide");
+        }else{
+            throw "error";
+        }
+    }).catch(err=>M.toast({html:"Error sending OTP to "+OTPModel.phone}));
 });
 
 $("#otp").on('keypress',(e)=>{
     OTPModel.OTP+=e.key;
-    if(OTPModel.OTP.length>4){
+    if(OTPModel.OTP.length>=4){
         $("#verifyOTP").removeClass("disabled");
     }else{
         $("#verifyOTP").addClass("disabled")
@@ -57,18 +256,24 @@ $("#otp").on('keypress',(e)=>{
 
 $("#verifyOTP").on("click",()=>{
     M.toast({html:`Verifying OTP Sent to ${OTPModel.phone}`});
-    setTimeout(()=>{   
-        $("#otp-verify-card").addClass("hide");
-        $($(".step .breadcrumb")[0]).remove();
-        $($(".step .breadcrumb")[0]).addClass("active");
-        $(".onboard-step-2").removeClass("hide");
-        $(".onboard-step-1").addClass("hide");
-        $(".step .breadcrumb").removeClass("hide");
-        M.toast({html:`OTP Verified.`});
-    },5000)
+    $.get("/api/verifyOTP/"+OTPModel.phone+"/"+OTPModel.OTP).then((res)=>{
+        if(res.status){
+            $("#otp-verify-card").addClass("hide");
+            $($(".step .breadcrumb")[0]).remove();
+            $($(".step .breadcrumb")[0]).addClass("active");
+            $(".onboard-step-2").removeClass("hide");
+            $(".onboard-step-1").addClass("hide");
+            $(".step .breadcrumb").removeClass("hide");
+            M.toast({html:`OTP Verified.`});
+            localStorage.setItem("userId",OTPModel.phone);
+        }else{
+            throw "error";
+        }
+    }).catch(err=>M.toast({html:"Error verifying OTP sent to "+OTPModel.phone}));
 });
 
 $("#next").on("click",()=>{
+    saveModel();
     $(".onboard-step-2").addClass("hide");
     $(".onboard-step-3").removeClass("hide");
 })
@@ -79,9 +284,13 @@ $("#edit-story").on("click",()=>{
 })
 
 $("#submit-review").on("click",()=>{
-    M.toast({html:"Loan application submited for review, hang tight !"})
-    $(".onboard-step-3").addClass("hide");
-    $(".onboard-step-4").removeClass("hide");
+    $.jpost("/submitReview",Info,"application/json").then((res)=>{    
+        M.toast({html:"Loan application submited for review, hang tight !"})
+        $(".onboard-step-3").addClass("hide");
+        $(".onboard-step-4").removeClass("hide");
+    }).catch(()=>{
+        M.toast({html:"Error submitting application"});
+    });
 })
 
 $("#edit-info").on("click",()=>{
@@ -100,8 +309,8 @@ $("#back-to-step-5").on("click",()=>{
 })
 
 $("#confirm-offer-term").on("click",()=>{
-    $($(".step .breadcrumb")[0]).removeClass("active");
-    $($(".step .breadcrumb")[1]).addClass("active");
-    $(".onboard-step-6").addClass("hide");
-    $(".onboard-step-7").removeClass("hide");
-})
+    Info.loan.confirmed = true;
+    saveModel(true);
+});
+
+getModel();
